804398821f17572ac7ad090d2822a3d249220467
bkdhanush <bkdhanushofficial@gmail.com>
2022-10-29 16:39:09 +0530

    style commit

--- src/routes/city.js
+++ src/routes/city.js
@@ -16,29 +16,16 @@ router.get('/:id', async (req, res) => {
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
-    https.get(url,function(response){
-        console.log(response.statusCode);
-        
-               //sendingresponse on website from server
+    https.get(url,function(response){        
                 response.on("data",function(data){
-                //getting data as json format
                 const weatherData = JSON.parse(data);
-                //fetching specific things we need
                 const temp = weatherData.main.temp;
                 const weatherDescription = weatherData.weather[0].weatherDescription;
                 const icon = weatherData.weather[0].icon;
-                //image url for icons fetched from weather API
                 const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-                //can have many res.write but one res.send and sending back in html format to browser
-                //res.write("<p>The weather is currently " + weatherDescription + "<p>");
-                //change the cityName from London to query
-                // res.write("<h1>The temperature in " + query + " is " + temp + " degree Celcius.</h1> ");
-                // res.write("<img src=" + imageURL +">");
-                // res.send();
                 res.render('city', { city, places,temp,imageURL });
 
         })
-
     })
 });
 
